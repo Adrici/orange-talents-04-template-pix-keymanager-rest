36489a4dab1a1921cb0ab26c22a7f7a3c4382e4e
Adrici <cirellia5@gmail.com>
2021-06-28 18:01:39 -0300

    Expondo para o frontend: Remoção de uma chave Pix existente

--- pix-key-manager-rest-api/src/main/kotlin/br/com/zup/edu/shared/KeyManagerGrpcFactory.kt
+++ pix-key-manager-rest-api/src/main/kotlin/br/com/zup/edu/shared/KeyManagerGrpcFactory.kt
@@ -2,6 +2,7 @@ package br.com.zup.edu.shared
 
 
 import br.com.zup.grpc.KeymanagerRegistraGrpcServiceGrpc
+import br.com.zup.grpc.KeymanagerRemoveGrpcServiceGrpc
 import io.grpc.ManagedChannel
 import io.micronaut.context.annotation.Factory
 import io.micronaut.grpc.annotation.GrpcChannel
@@ -11,4 +12,7 @@ import javax.inject.Singleton
 class KeyManagerGrpcFactory(@GrpcChannel("KeyManager") val channel: ManagedChannel) {
     @Singleton
     fun registraChave() = KeymanagerRegistraGrpcServiceGrpc.newBlockingStub(channel)
+
+    @Singleton
+    fun removeChave() = KeymanagerRemoveGrpcServiceGrpc.newBlockingStub(channel)
 }
